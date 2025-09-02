870f34bdcfa5230ad9a2025c070bc2bd5439fdb8
Braian-92 <zamudiobraianhernan@gmail.com>
2025-09-01 22:44:56 -0300

    extender funcionalidad de clase heredada

--- basico/oop/class.php
+++ basico/oop/class.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 
 $sale = new Sale(10.5, date('y-m-d'));
+$onlineSale = new OnlineSale(15, date('y-m-d'), 'Tarjeta');
+echo $onlineSale->createInvoice() . ' - desde onlineSale<br/>';
+echo $onlineSale->showInfo() . ' - desde onlineSale<br/>';
 $sale = new Sale(10.5, date('y-m-d'));
 echo Sale::$count . ' - estatic <br/>';
 //! eventos globales, se aplica a todas las instancias realizadas
@@ -17,7 +20,7 @@ echo '<pre>';
 var_dump($sale);
 echo '</pre>';
 
-echo $sale->createInvoice();
+echo $sale->createInvoice() . '<br/>';
 
 $concept = new Concept("cerveza", 10);
 $sale->addConcept($concept);
@@ -55,7 +58,20 @@ class Sale {
   }
 
   public function createInvoice(): string{
-    return "Se crea la factura.<br/>";
+    return "Se crea la factura.";
+  }
+}
+
+class OnlineSale extends Sale{
+  public string $paymentMethod;
+
+  public function __construct(float $total, string $date, string $paymentMethod){
+    parent::__construct($total, $date);
+    $this->paymentMethod = $paymentMethod;
+  }
+
+  public function showInfo(): string{
+    return "La venta tiene un monto de $this->total";
   }
 }
 
