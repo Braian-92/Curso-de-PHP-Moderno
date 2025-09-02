c8efaf1d5248513b7e4ad24f92fb03c7ee076ffc
Braian-92 <zamudiobraianhernan@gmail.com>
2025-09-01 22:22:41 -0300

    propiedades y eventos estaticos

--- basico/oop/class.php
+++ basico/oop/class.php
@@ -1,6 +1,14 @@
 <?php
 
 $sale = new Sale(10.5, date('y-m-d'));
+$sale = new Sale(10.5, date('y-m-d'));
+echo Sale::$count . ' - estatic <br/>';
+//! eventos globales, se aplica a todas las instancias realizadas
+Sale::reset();
+$sale = new Sale(10.5, date('y-m-d'));
+echo Sale::$count . ' - estatic <br/>';
+
+
 $sale->total = 11;
 $sale->date = date('y');
 
@@ -13,15 +21,25 @@ $sale->createInvoice();
 class Sale {
   public $total;
   public $date;
+  //! propiedad estatica
+  public static $count;
 
   public function __construct($total, $date){
+    echo 'Se ha creado el objeto.<br/>';
     $this->total = $total;
     $this->date = $date;
+    self::$count++;
   }
 
   public function __destruct(){
     echo 'Se ha eliminado el objeto.<br/>';
   }
+
+  public static function reset(){
+    echo 'Se ha reseteado el objeto.<br/>';
+    self::$count = 0;
+  }
+
   public function createInvoice(){
     echo "Se crea la factura.<br/>";
   }
