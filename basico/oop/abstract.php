b2d8ab2e9d261f2f5fb3c6270977f3da7d4eca32
Braian-92 <zamudiobraianhernan@gmail.com>
2025-09-01 23:41:59 -0300

    implantar modelo de clase abstracta

--- basico/oop/abstract.php
+++ basico/oop/abstract.php
@@ -1,5 +1,16 @@
 <?php
 //! las clases abstractas son moldes de clases
+//! se utilizan para diseño de software
+// $product = new Product(); //! ERROR: no se puede instanciar una clase abtracta
+
+$beer = new Beer("Delirium", 15);
+echo $beer->getName();
+
+showInfo($beer);
+
+function showInfo(Product $product){
+  echo " $ ".$product->calculatePrice();
+}
 
 abstract class Product {
   protected float $price;
@@ -9,5 +20,18 @@ abstract class Product {
 
   public function getName(): string{
     return $this->name;
+  }
+}
+
+class Beer extends Product {
+  const TAX = 1.1; //! las constantes van en mayusculas
+
+  public function __construct($name, $price){
+    $this->name = $name;
+    $this->price = $price;
+  }
+
+  public function calculatePrice(): float {
+    return $this->price * self::TAX;
   }
 }
