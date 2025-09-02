f59223fc7e0a38c8d2fbacaea19bafe5c8d90e86
Braian-92 <zamudiobraianhernan@gmail.com>
2025-09-01 22:31:41 -0300

    instancia anidada

--- basico/oop/class.php
+++ basico/oop/class.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types=1);
 
 $sale = new Sale(10.5, date('y-m-d'));
 $sale = new Sale(10.5, date('y-m-d'));
 echo Sale::$count . ' - estatic <br/>';
 //! eventos globales, se aplica a todas las instancias realizadas
-Sale::reset();
+echo Sale::reset();
 $sale = new Sale(10.5, date('y-m-d'));
 echo Sale::$count . ' - estatic <br/>';
 
@@ -16,18 +17,27 @@ echo '<pre>';
 var_dump($sale);
 echo '</pre>';
 
-$sale->createInvoice();
+echo $sale->createInvoice();
+
+$concept = new Concept("cerveza", 10);
+$sale->addConcept($concept);
+
+echo '<pre>';
+var_dump($sale->concepts);
+echo '</pre>';
 
 class Sale {
-  public $total;
-  public $date;
+  public float $total;
+  public string $date;
+  public array $concepts;
   //! propiedad estatica
   public static $count;
 
-  public function __construct($total, $date){
+  public function __construct(float $total, string $date){
     echo 'Se ha creado el objeto.<br/>';
     $this->total = $total;
     $this->date = $date;
+    $this->concepts = [];
     self::$count++;
   }
 
@@ -35,12 +45,26 @@ class Sale {
     echo 'Se ha eliminado el objeto.<br/>';
   }
 
-  public static function reset(){
-    echo 'Se ha reseteado el objeto.<br/>';
+  public static function reset(): string{
+    return 'Se ha reseteado el objeto.<br/>';
     self::$count = 0;
   }
 
-  public function createInvoice(){
-    echo "Se crea la factura.<br/>";
+  public function addConcept(Concept $concept){
+    $this->concepts[] = $concept;
+  }
+
+  public function createInvoice(): string{
+    return "Se crea la factura.<br/>";
+  }
+}
+
+class Concept {
+  public string $description;
+  public float $amount;
+
+  public function __construct(string $description, float $amount){
+    $this->description = $description;
+    $this->amount = $amount;
   }
 }
