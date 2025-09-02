324f26f6e6176ea820aab4db8a7802705931e1dc
Braian-92 <zamudiobraianhernan@gmail.com>
2025-09-01 22:16:18 -0300

    funciones en clases, constructores y destructores

--- basico/oop/class.php
+++ basico/oop/class.php
@@ -1,14 +1,28 @@
 <?php
 
-$sale = new Sale();
-$sale->total = 10.5;
-$sale->date = date('y-m-d');
+$sale = new Sale(10.5, date('y-m-d'));
+$sale->total = 11;
+$sale->date = date('y');
 
 echo '<pre>';
 var_dump($sale);
 echo '</pre>';
 
+$sale->createInvoice();
+
 class Sale {
   public $total;
   public $date;
+
+  public function __construct($total, $date){
+    $this->total = $total;
+    $this->date = $date;
+  }
+
+  public function __destruct(){
+    echo 'Se ha eliminado el objeto.<br/>';
+  }
+  public function createInvoice(){
+    echo "Se crea la factura.<br/>";
+  }
 }
