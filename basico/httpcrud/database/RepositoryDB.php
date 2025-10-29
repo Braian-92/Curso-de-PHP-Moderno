ae7bcf76e07b19290172f847f75bd16fc53011e6
Braian-92 <zamudiobraianhernan@gmail.com>
2025-10-28 23:24:28 -0300

    docu extends e implements

--- basico/httpcrud/database/RepositoryDB.php
+++ basico/httpcrud/database/RepositoryDB.php
@@ -6,6 +6,31 @@ use PDO;
 use app\interfaces\RepositoryInterface;
 use app\Database\BaseRepository;
 
+/**
+ * EXPLICACIÓN DE EXTENDS E IMPLEMENTS:
+ * 
+ * EXTENDS (Herencia):
+ * - RepositoryDB EXTENDS BaseRepository
+ * - Significa que RepositoryDB "hereda" de BaseRepository
+ * - Obtiene automáticamente todas las propiedades y métodos de BaseRepository
+ * - En este caso: hereda la propiedad $pdo y el constructor que establece la conexión a la BD
+ * - NO es obligatorio implementar métodos (BaseRepository es abstracta pero no tiene métodos abstractos)
+ * 
+ * IMPLEMENTS (Contrato):
+ * - RepositoryDB IMPLEMENTS RepositoryInterface
+ * - Significa que RepositoryDB debe cumplir un "contrato" definido en RepositoryInterface
+ * - OBLIGA a implementar TODOS los métodos definidos en la interfaz:
+ *   - create($data)
+ *   - get(): array
+ *   - update($data)
+ *   - delete(int $id)
+ *   - exists(int $id): bool
+ * - Si no implementas alguno de estos métodos, PHP dará error fatal
+ * 
+ * RESUMEN:
+ * - EXTENDS = "Soy hijo de..." (herencia, obtienes funcionalidad)
+ * - IMPLEMENTS = "Me comprometo a..." (contrato, debes cumplir)
+ */
 class RepositoryDB extends BaseRepository implements RepositoryInterface{
   const TABLE = 'beer';
 
